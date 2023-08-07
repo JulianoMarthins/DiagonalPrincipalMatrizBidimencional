15aa1122c85fc742d7a4091048df79fbae8b1dd9
Juliano Martins de Souza <99154908+JulianoMarthins@users.noreply.github.com>
2023-08-07 17:56:52 -0300

    ver_01
    Exercicio de fixação da DevSuperior sobre Matriz BiDimencional.
    
    O programa recebe a entrada do usuários sobre quantas linhas/colunas a matriz bi-dimencional terá.
    
    É criado um vetor dentro de outro vetor, o usuário precisará inserir todos os valores do vetor, sendo eles negativos ou positivos.
    
    Após, o programa retornará a diagonal principal e a quantidade de números negativos digitados.

---
+++ Matriz/src/programa/Main.java
@@ -0,0 +1,42 @@
+package programa;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		;
+		System.out.print("Matriz de quantos lados? ");
+		int x = sc.nextInt();
+
+		int[][] vetor = new int[x][x];
+
+		for (int i = 0; i < vetor.length; i++) {
+			for (int j = 0; j < vetor[i].length; j++) {
+				System.out.printf("Digite a linha %d, coluna %d: ", (i + 1), (j + 1), "\n");
+				vetor[i][j] = sc.nextInt();
+			}
+		}
+
+		System.out.println("Diagonal principal");
+		int contador = 0;
+		for (int i = 0; i < vetor.length; i++) {
+			for (int j = 0; j < vetor[i].length; j++) {
+				if (vetor[i][j] < 0) {
+					contador++;
+				}
+				if (i == j) {
+					System.out.println(vetor[i][j]);
+				}
+
+			}
+
+		}
+		System.out.println("\nNúmeros negativos: ");
+
+		System.out.println(contador);
+		sc.close();
+	}
+
+}
